dd2fd5d9a353484b57511114b88eb2ed5a698e1f
HugoNovais <hugo.o.novais@outlook.com>
2023-05-03 21:26:02 -0300

    instalando e configurando

---
+++ config/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    dialect: "postgres",
+    host: "127.0.0.1",
+    username:"postgres",
+    password: "200711300",
+    database: "curso_sequelize",
+    define: {
+        timestamps: true,
+    },
+};
